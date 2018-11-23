599ba2bbe1f9b6b2fa1ca2b68f594063cdd197e7
doddthomas <doddthomas@cfl.rr.com>
2018-11-23 06:07:14 -0600

    original dodd ex checkin

---
+++ while_loop_ex.py
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------
+#
+#   while_loop_ex.py - a python script showing example of while loop
+#
+#---------------------    module history   -----------------------------
+#
+#   12-OCT-18 : PTR XXXX, Created, Dodd M. Thomas
+#
+#-----------------------------------------------------------------------
+ 
+spam = 0
+while spam < 5:
+    print('inside loop with spam=>' + str(spam))
+    spam = spam +1
+print('after loop with spam=>' + str(spam))
