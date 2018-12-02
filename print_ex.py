0ba4cbba2efb8c7f20d2486f99286355fd46b6de
loganthomas <logan.thomas005@gmail.com>
2018-12-01 20:56:25 -0600

    Added examples for %-formatting, str.format(), and f-strings
    Python offers a wide variety of how to print objects.
    The best approach is to use f-strings as they are fast
    and have some great attributes. Since f-strings are available
    in Python 3.6 and above, you may have to revert to using
    str.format() in some cases. It is recommended to not use
    %-formatting anymore.
    
    Closes: #7

--- print_ex.py
+++ print_ex.py
@@ -1,4 +1,78 @@
 """
-print_ex.py - a python script showing examples of various
-ways to print objects in Python.
+Typically, a module has a description at the top giving a brief summary
+of its functionality and/or some examples. This is housed in a `docstring`
+(a block of text surrounded by three double quotation marks). Below is an
+example of what I would write for this script:
+
+Examples of how to print objects in Python.
+
+This script demonstrates a few methods for printing objects in Python.
+
+Topics covered:
+    - %-formatting
+    - str.format()
+    - f.strings
+
+TODO:
+    * Code review this type of documentation.
+    * Remove below doc and replace with this. Or, vice versa.
 """
+
+#-----------------------------------------------------------------------
+#
+#   print_ex.py - a python script showing examples of printing
+#                 objects in Python.
+#
+#---------------------    module history   -----------------------------
+#
+#   01-DEC-18 : PTR XXXX, Created, Logan Thomas
+#
+#-----------------------------------------------------------------------
+
+# Define dummy objects to print
+name = 'Tina'
+age = 35
+hams = 2.5
+
+# Old-school string formatting in Python: %-formatting
+# This is not recommended by the official Python docs
+print('Hello %s' % name)
+# yields => 'Hello Tina'
+
+print('Hello %s. You are %d years old and have eaten %.2f hams today.' % (name, age, hams))
+# yields => 'Hello Tina. You are 35 years old and have eaten 2.50 hams today.'
+
+
+
+# More recent string formatting (preferred instead of %-formatting)
+print('Hello {}'.format(name))
+# yields => 'Hello Tina'
+
+print('Hello {}. You are {} years old and have eaten {} hams today.'.format(name, age, hams))
+# yields => 'Hello Tina. You are 35 years old and have eaten 2.5 hams today.'
+
+# Can switch indexes to print with str.format
+print('Hello {0}. You are {2} years old and have eaten {1} hams today.'.format(name, age, hams))
+# yields => 'Hello Tina. You are 2.5 years old and have eaten 35 hams today.'
+
+
+
+# Most recent (Python 3.6 and above) and most preferred: f-strings.
+print(f'Hello {name}')
+# yields => 'Hello Tina'
+
+print(f'Hello {name}. You are {age} years old and have eaten {hams} hams today.')
+# yields => 'Hello Tina. You are 35 years old and have eaten 2.5 hams today.'
+
+# f-strings support arbitrary expressions
+print(f'{5*3}')
+# yields => 15
+
+# f-strings support function evaluations
+def shout(name):
+    return name.upper() + '!!!'
+
+
+print(f'{shout(name)} Eat your food!')
+# yields => 'TINA!!! Eat your food!'
+
