d05a63b7ec5d5718a463b08cb8487c0ae881e454
loganthomas <logan.thomas005@gmail.com>
2020-09-03 21:13:02 -0500

    [ENH] adds better documentation and seed setting examples

--- random_ex.py
+++ random_ex.py
@@ -11,11 +11,24 @@
 
 
 import random
+print('from random for loop:')
 for i in range(5):
     print(random.randint(1,100))
 
 
 import numpy as np
-np.random.randint(1,100,5)
+print('from numpy array generation:')
+print(np.random.randint(1,100,5))
 
-[random.randint(1,100) for i in range(5)]
+print('from random list comprehension:')
+rands = [random.randint(1,100) for i in range(5)]
+print(rands)
+
+# Can set seed with random or numpy to ensure state
+random.seed(42)
+print([random.random() for _ in range(5)])
+# ALWAYS yields => [0.6394267984578837, 0.025010755222666936, 0.27502931836911926, 0.22321073814882275, 0.7364712141640124]
+
+np.random.seed(5)
+print(np.random.randint(1,100,5))
+# ALWAYS yields => [79 62 17 74  9]
