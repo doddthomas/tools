b7b18b71f4a77dfb9eee6d43bb54c8f1e47b4172
loganthomas <logan.thomas005@gmail.com>
2020-09-03 21:06:42 -0500

    [FIX] fix import of numnpy as correct numpy

--- random_ex.py
+++ random_ex.py
@@ -1,21 +1,21 @@
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #
 #   random_ex.py - a python script showing example of random number
 #                  generator
 #
-#---------------------    module history   -----------------------------
+# ---------------------    module history   -----------------------------
 #
 #   12-OCT-18 : PTR XXXX, Created, Dodd M. Thomas
 #
-#-----------------------------------------------------------------------
- 
- 
+# -----------------------------------------------------------------------
+
+
 import random
 for i in range(5):
-   print(random.randint(1,100))
+    print(random.randint(1,100))
 
 
-import numnpy as np
+import numpy as np
 np.random.randint(1,100,5)
 
 [random.randint(1,100) for i in range(5)]
